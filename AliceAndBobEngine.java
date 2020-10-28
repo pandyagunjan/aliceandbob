c6dc1e4932e4bb0bc0e1f19c51f5e5763428c41b
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-10-27 22:59:50 -0400

    added by gunajn

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -16,7 +16,17 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
-        return result;
-    }
+  
+      if(input=="Alice")
+      result = "Hello Alice";
+      else 
+      if(input=="Bob")
+      result = "Hello Bob";
+      else      
+      result = "I DON'T KNOW YOU, YOU ARE SCARY"; 
+      
+      System.out.println(input);
+      return result;
+}
+
 }
